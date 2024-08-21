ebfa2ea72d09f50c4886cc2bf3cb7e75d06af787
amintorbat <98237026+amintorbat@users.noreply.github.com>
2024-08-21 21:34:39 +0330

    change

--- script.js
+++ script.js
@@ -3,4 +3,29 @@ const note = document.querySelector(".nowplaying");
 const hints = document.querySelectorAll(".hints");
 
 function playNote(e) {
-  const audio = document.querySelector(`audio[data-key="${e.k
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!key) return;
+
+  const keyNote = key.getAttribute("data-note");
+
+  key.classList.add("playing");
+  note.innerHTML = keyNote;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const hintsOn = (e, index) => {
+  e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+};
+hints.forEach(hintsOn);
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playNote);
